eb92f957b6a24d3b0d8e64640a679c67f1e340ce
Hackerboy68 <163637208+Hakerboy68@users.noreply.github.com>
2024-03-23 21:20:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,15 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
-  [''] 
-] //Number of owners
+  ['+919881459658', 'Atharva', true],
+  ['+91 82920 01437', 'Ash', true],
+ //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['919881459658','+91 82920 01437'] 
+global.prems = ['919881459658', '+91 82920 01437']
+global.allowed = ['919881459658', '+91 82920 01437']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -48,9 +47,8 @@ global.APIKeys = { // APIKey Here
   'https://zenzapis.xyz': `${keysxxx}`, 
   'https://api-fgmods.ddns.net': 'fg-dylux'
 }
-
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'RAJ MD'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
